b76ad3d4c33c56d874fea3cc8f2ea91bfce8fd98
Tobias Brink <brink@mm.tu-darmstadt.de>
2013-12-21 15:09:09 +0100

    Correct licensing headers in scripting plugin.

--- src/plugins/scripting/ui/ScriptingApplet.cpp
+++ src/plugins/scripting/ui/ScriptingApplet.cpp
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (2013) Alexander Stukowski
+//  Copyright (2013) Alexander Stukowski, Tobias Brink
 //
 //  This file is part of OVITO (Open Visualization Tool).
 //
